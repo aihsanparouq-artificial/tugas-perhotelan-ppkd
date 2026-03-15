65fb7af7d5c3a7349083cb627ce71c9c96483769
Abdullah Ihsan Parouq <aihsanparouq@gmail.com>
2026-03-15 09:13:57 +0700

    Initial Laravel hotel registration project

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GuestController;
+
 
 Route::get('/', function () {
     return view('welcome');
-});
+    });
+    Route::get('/', [GuestController::class,'index']);
+    
+    Route::get('/register', [GuestController::class,'create']);
+    Route::post('/register', [GuestController::class,'store']);
+    
+    Route::get('/guests', [GuestController::class,'index']);
+    Route::get('/guests/{id}', [GuestController::class,'show']);
