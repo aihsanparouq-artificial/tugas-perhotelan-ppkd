f52e23e3bf967f7a5156139945a869e15df3feca
raisyaedria26 <raisyaedria26@gmail.com>
2026-03-16 14:04:20 +0700

    las senin

--- resources/views/guests/print.blade.php
+++ resources/views/guests/print.blade.php
@@ -85,13 +85,13 @@
                 <td>Company / Agent</td>
                 <td>{{ $guest->company }}</td>
                 <td>Fax</td>
-                <td>: -</td>
+                <td>-</td>
             </tr>
             <tr>
                 <td>Booking No</td>
                 <td>{{ $guest->id }}</td>
                 <td>Email</td>
-                <td>: {{ $guest->email }}</td>
+                <td>{{ $guest->email }}</td>
             </tr>
             <tr>
                 <td>Book By</td>
@@ -101,97 +101,66 @@
             </tr>
         </table>
             <hr>
-
-<div class="section">
-
-<table>
-
-<tr>
-<td width="30%">First Name</td>
-<td>: {{ $guest->name }}</td>
-</tr>
-
-<tr>
-<td>Arrival Date</td>
-<td>: {{ $guest->arrival_date }}</td>
-</tr>
-
-<tr>
-<td>Departure Date</td>
-<td>: {{ $guest->departure_date }}</td>
-</tr>
-
-<tr>
-<td>Total Night</td>
-<td>: -</td>
-</tr>
-
-<tr>
-<td>Room / Unit Type</td>
-<td>: {{ $guest->room_type }}</td>
-</tr>
-
-<tr>
-<td>Person Pax</td>
-<td>: {{ $guest->no_of_person }}</td>
-</tr>
-
-<tr>
-<td>Room Rate Net</td>
-<td>: -</td>
-</tr>
-
-</table>
-
-</div>
-
-<hr>
-
-<p>
-Please guarantee this booking with credit card number with clear copy of the card both sides and card holder signature.
-</p>
-
-<p>
-Please settle your outstanding to our account.
-</p>
-
-<div class="section">
-
-<b>Bank Transfer</b>
-
-<table>
-
-<tr>
-<td width="30%">Mandiri Account</td>
-<td>: -</td>
-</tr>
-
-<tr>
-<td>Mandiri Name Account</td>
-<td>: PPKD HOTEL</td>
-</tr>
-
-</table>
-
-</div>
-
-<hr>
-
-<div class="section">
-
-<b>Cancellation policy:</b>
-
-<ol>
-
-<li>Check in time is 02.00 pm and check out time 12.00 pm.</li>
-
-<li>All non guaranteed reservations will automatically be released on 6 pm.</li>
-
-<li>The hotel will charge 1 night for guaranteed reservations not cancelled before arrival.</li>
-
-</ol>
-
-</div>
-
-</body>
-</html>
+            <div class="section">
+                <table>
+                    <tr>
+                        <td width="30%">First Name</td>
+                        <td>: {{ $guest->name }}</td>
+                    </tr>
+                    <tr>
+                        <td>Arrival Date</td>
+                        <td>: {{ $guest->arrival_date }}</td>
+                    </tr>
+                    <tr>
+                        <td>Departure Date</td>
+                        <td>: {{ $guest->departure_date }}</td>
+                    </tr>
+                    <tr>
+                        <td>Total Night</td>
+                        <td>: -</td>
+                    </tr>
+                    <tr>
+                        <td>Room / Unit Type</td>
+                        <td>: {{ $guest->room_type }}</td>
+                    </tr>
+                    <tr>
+                        <td>Person Pax</td>
+                        <td>: {{ $guest->no_of_person }}</td>
+                    </tr>
+                    <tr>
+                        <td>Room Rate Net</td>
+                        <td>: -</td>
+                    </tr>
+                </table>
+            </div>
+            <hr>
+            <p>
+                Please guarantee this booking with credit card number with clear copy of the card both sides and card holder signature.
+            </p>
+            <p>
+                Please settle your outstanding to our account.
+            </p>
+            <div class="section">
+                <b>Bank Transfer</b>
+                <table>
+                    <tr>
+                        <td width="30%">Mandiri Account</td>
+                        <td>: -</td>
+                    </tr>
+                    <tr>
+                        <td>Mandiri Name Account</td>
+                        <td>: PPKD HOTEL</td>
+                    </tr>
+                </table>
+            </div>
+            <hr>
+            <div class="section">
+                <b>Cancellation policy:</b>
+                <ol>
+                    <li>Check in time is 02.00 pm and check out time 12.00 pm.</li>
+                    <li>All non guaranteed reservations will automatically be released on 6 pm.</li>
+                    <li>The hotel will charge 1 night for guaranteed reservations not cancelled before arrival.</li>
+                </ol>
+            </div>
+        </body>
+        </html>
